f4b9120b8b4b8ee54e3662d7f6f51ba345999497
Nadezhda Georgieva <nadezhda.georgieva2@gmail.com>
2021-05-28 00:09:55 +0300

    Update 23-Countries-and-Currency-Euro-Not Euro.sql

--- 02_BASIC_CRUD/Exercise/23-Countries-and-Currency-Euro-Not Euro.sql
+++ 02_BASIC_CRUD/Exercise/23-Countries-and-Currency-Euro-Not Euro.sql
@@ -1,6 +1,6 @@
 SELECT `country_name`, `country_code`, 
 CASE 
-	WHEN `currency_code` = 'EUR' THEN 'Euro'
+    WHEN `currency_code` = 'EUR' THEN 'Euro'
     ELSE 'Not Euro'
 END AS `currency` FROM `countries`
 ORDER BY `country_name` ASC;
