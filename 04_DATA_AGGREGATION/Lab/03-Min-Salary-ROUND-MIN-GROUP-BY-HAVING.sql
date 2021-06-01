702c0f90d85ace43d931e93344cb8a0f32e1856f
CrystallizedSnowflakes <nadezhda.georgieva2@gmail.com>
2021-06-01 18:38:16 +0300

    added Lab into 04_DATA_AGGREGATION

---
+++ 04_DATA_AGGREGATION/Lab/03-Min-Salary-ROUND-MIN-GROUP-BY-HAVING.sql
@@ -0,0 +1,3 @@
+SELECT `department_id`, ROUND(MIN(`salary`), 2) AS `Min Salary` FROM `employees`
+GROUP BY `department_id`
+HAVING `Min Salary` > 800;
---
+++ 04_DATA_AGGREGATION/Lab/04-Appetizers-Count-COUNT-WHERE.sql
@@ -0,0 +1,2 @@
+SELECT COUNT(`category_id`) AS `Count` FROM `products`
+WHERE `category_id` = 2 AND `price` > 8;
