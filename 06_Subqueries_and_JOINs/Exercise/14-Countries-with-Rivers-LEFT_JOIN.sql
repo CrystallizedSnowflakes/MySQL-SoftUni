24d413329a1226da83fbe58d32e9384bb1c28ef4
CrystallizedSnowflakes <nadezhda.georgieva2@gmail.com>
2021-06-10 20:51:33 +0300

    delete files

--- 06_Subqueries_and_JOINs/Exercise/14-Countries-with-Rivers-LEFT_JOIN.sql
+++ 06_Subqueries_and_JOINs/Exercise/14-Countries-with-Rivers-LEFT_JOIN.sql
@@ -18,19 +18,3 @@ WHERE
     continent_name = 'Africa'
 ORDER BY c.country_name
 LIMIT 5;
-
-
-SELECT 
-    c.country_name, r.river_name
-FROM
-    countries AS c
-        LEFT JOIN
-    countries_rivers AS cr ON c.country_code = cr.country_code
-        LEFT JOIN
-    rivers AS r ON cr.river_id = r.id
-        JOIN
-    continents ON c.continent_code = continents.continent_code
-WHERE
-    continent_name = 'Africa'
-ORDER BY c.country_name
-LIMIT 5;
