086e4fa39e8b63f67523535b4b9d9453047749a4
CrystallizedSnowflakes <nadezhda.georgieva2@gmail.com>
2021-06-10 21:13:22 +0300

    some changes

--- 05_TABLE_RELATIONS/Exercise/09-Peaks-in-Rila-JOIN_ON.sql
+++ 05_TABLE_RELATIONS/Exercise/09-Peaks-in-Rila-JOIN_ON.sql
@@ -1,6 +1,11 @@
-SELECT m.`mountain_range`, p.`peak_name`, p.`elevation` AS `peak_elevation` 
-FROM `mountains` AS m 
-JOIN `peaks` AS p
-ON m.`id` = p.`mountain_id`
-WHERE m.`mountain_range` = 'Rila'
+SELECT 
+    m.`mountain_range`,
+    p.`peak_name`,
+    p.`elevation` AS `peak_elevation`
+FROM
+    `mountains` AS m
+        JOIN
+    `peaks` AS p ON m.`id` = p.`mountain_id`
+WHERE
+    m.`mountain_range` = 'Rila'
 ORDER BY `peak_elevation` DESC;
---
+++ 05_TABLE_RELATIONS/README.md
@@ -0,0 +1,10 @@
+Geography Database
+
+![alt text](https://github.com/CrystallizedSnowflakes/SQL-SoftUni/tree/main/05_TABLE_RELATIONS/Geography_DB.jpg)
+
+
+
+
+Soft_Uni Database
+
+![alt text](https://github.com/CrystallizedSnowflakes/SQL-SoftUni/tree/main/05_TABLE_RELATIONS/Soft_Uni_DB.jpg)
