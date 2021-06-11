96e1a4c767c45cc96e0b9393767b34c9d3a8e63f
Nadezhda Georgieva <nadezhda.georgieva2@gmail.com>
2021-06-11 15:58:34 +0300

    Update 01-Data-INSERT_INTO-by-SUBQUERY.sql

--- MySQL-Exam-21-Oct-2018/01-Data-INSERT_INTO-by-SUBQUERY.sql
+++ MySQL-Exam-21-Oct-2018/01-Data-INSERT_INTO-by-SUBQUERY.sql
@@ -11,23 +11,23 @@
 
 INSERT INTO travel_cards (card_number, job_during_journey, colonist_id, journey_id)
 	SELECT
-		(
-			IF( c.birth_date > '1980-01-01', 
-				concat_ws('', YEAR(c.birth_date), DAY(c.birth_date), SUBSTRING(c.ucn, 1, 4)),
-				concat_ws('', YEAR(c.birth_date), MONTH(c.birth_date), SUBSTRING(c.ucn, 7))
-            )
-		) AS card_number,
+	(
+		IF( c.birth_date > '1980-01-01', 
+			concat_ws('', YEAR(c.birth_date), DAY(c.birth_date), SUBSTRING(c.ucn, 1, 4)),
+			concat_ws('', YEAR(c.birth_date), MONTH(c.birth_date), SUBSTRING(c.ucn, 7))
+        )
+	) AS card_number,
         (
-			CASE
-				WHEN c.id % 2 = 0 THEN 'Pilot'
-				WHEN c.id % 3 = 0 THEN 'Cook'
-				ELSE 'Engineer'
-			END        
+		CASE
+			WHEN c.id % 2 = 0 THEN 'Pilot'
+			WHEN c.id % 3 = 0 THEN 'Cook'
+			ELSE 'Engineer'
+		END        
         ) AS job_during_journay,
         c.id,
         (
-			LEFT(c.ucn, 1)
+		LEFT(c.ucn, 1)
         ) AS journey_id    
 	FROM colonists AS c
-    WHERE c.id BETWEEN 96 AND 100;
+    	WHERE c.id BETWEEN 96 AND 100;
 
