dcde5fd0e3e68ff890b22a09579ffd11dbed66a6
CrystallizedSnowflakes <nadezhda.georgieva2@gmail.com>
2021-06-17 12:13:16 +0300

    rename file

--- 03_BUILT-IN_FUNCTIONS/Exercise/16-Orders-Table-TIMESTAMPADD.sql
+++ 03_BUILT-IN_FUNCTIONS/Exercise/16-Orders-Table-TIMESTAMPADD.sql
@@ -1,4 +1,4 @@
-SELECT `product_name`, `order_date`,
-DATE_ADD(`order_date`, INTERVAL 3 DAY) AS 'pay_due',
-DATE_ADD(`order_date`, INTERVAL 1 MONTH) AS 'deliver_due'
-FROM `orders`;
+SELECT product_name, order_date,
+TIMESTAMPADD(DAY, 3, order_date) AS 'pay_due',
+TIMESTAMPADD(MONTH, 1, order_date) AS 'deliver_due'
+FROM orders;
