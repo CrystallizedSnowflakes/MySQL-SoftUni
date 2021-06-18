5f0aa2d3d993f4b13cb6630a4cfadc66411e4f6e
Nadezhda Georgieva <nadezhda.georgieva2@gmail.com>
2021-06-18 12:56:59 +0300

    Update 17-Salary-Challenge-NESTED-SELECT.sql

--- 04_DATA_AGGREGATION/Exercise/17-Salary-Challenge-NESTED-SELECT.sql
+++ 04_DATA_AGGREGATION/Exercise/17-Salary-Challenge-NESTED-SELECT.sql
@@ -1,6 +1,6 @@
 SELECT e.`first_name`, e.`last_name`, e.`department_id` FROM `employees` AS e
 WHERE `salary` > (
-	SELECT AVG(e2.`salary`) FROM `employees` AS e2
+    SELECT AVG(e2.`salary`) FROM `employees` AS e2
     WHERE e2.`department_id` = e.`department_id`
     GROUP BY e2.`department_id`)
 ORDER BY e.`department_id`, e.`employee_id`
