c6f38aa2ad10b4023d429a7a4a895babce75adcc
CrystallizedSnowflakes <nadezhda.georgieva2@gmail.com>
2021-06-19 20:10:33 +0300

    added a new schema

--- MySQL-Exam-9-Jun-2019/03-UPDATE-JOIN-SET-WHERE.sql
+++ MySQL-Exam-9-Jun-2019/03-UPDATE-JOIN-SET-WHERE.sql
@@ -4,11 +4,11 @@
 
 UPDATE employees_clients as ec
 JOIN
-(SELECT ec2.employee_id, COUNT(ec2.client_id) AS 'clients_count'
-		FROM employees_clients as ec2 
-		GROUP BY ec2.employee_id
-		ORDER BY clients_count, ec2.employee_id
-        LIMIT 1) AS tbl
+	(SELECT ec2.employee_id, COUNT(ec2.client_id) AS 'clients_count'
+	FROM employees_clients as ec2 
+	GROUP BY ec2.employee_id
+	ORDER BY clients_count, ec2.employee_id
+	LIMIT 1) AS tbl
 SET ec.employee_id = tbl.employee_id
 WHERE ec.employee_id = ec.client_id;
 
@@ -22,4 +22,15 @@ SET ec.employee_id = (SELECT tbl.employee_id
 			GROUP BY ec2.employee_id
 			ORDER BY COUNT(ec2.client_id), ec2.employee_id
 			LIMIT 1) AS tbl)
+WHERE ec.employee_id = ec.client_id;
+
+-- --------------------------------------------------------
+
+UPDATE employees_clients AS ec
+SET ec.employee_id = (
+	SELECT tbl.employee_id FROM (SELECT * FROM employees_clients) AS tbl
+    GROUP BY employee_id
+    ORDER BY COUNT(tbl.client_id) ASC, tbl. employee_id ASC
+    LIMIT 1
+)
 WHERE ec.employee_id = ec.client_id;
